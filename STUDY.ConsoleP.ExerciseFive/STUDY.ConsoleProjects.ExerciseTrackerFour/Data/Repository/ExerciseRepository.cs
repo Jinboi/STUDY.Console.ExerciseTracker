84fb5433b1f73ff05f5da69b4251de90a14c830c
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-05-03 00:20:35 -0700

    added Repo and connected AddExerciseEntry

---
+++ STUDY.ConsoleP.ExerciseFive/STUDY.ConsoleProjects.ExerciseTrackerFour/Data/Repository/ExerciseRepository.cs
@@ -0,0 +1,36 @@
+﻿using STUDY.ConsoleProjects.ExerciseTrackerFour.Models;
+
+namespace STUDY.ConsoleProjects.ExerciseTrackerFour.Data.Repository;
+internal class ExerciseRepository : IExerciseRepository
+{
+    private readonly ExerciseDbContext _context;
+    public ExerciseRepository(ExerciseDbContext context)
+    {
+        _context = context;
+    }
+    public void AddExerciseEntry(Exercise exercise)
+    {
+        _context.Exercises.Add(exercise);
+        _context.SaveChanges();
+    }
+
+    public void DeleteExerciseEntry()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateExerciseEntry()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ViewAllExerciseEntries()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ViewSpecificExerciseEntry()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ STUDY.ConsoleP.ExerciseFive/STUDY.ConsoleProjects.ExerciseTrackerFour/Data/Repository/IExerciseRepository.cs
@@ -0,0 +1,11 @@
+﻿using STUDY.ConsoleProjects.ExerciseTrackerFour.Models;
+
+namespace STUDY.ConsoleProjects.ExerciseTrackerFour.Data.Repository;
+internal interface IExerciseRepository
+{
+    void ViewAllExerciseEntries();
+    void ViewSpecificExerciseEntry();
+    void AddExerciseEntry(Exercise exercise);
+    void UpdateExerciseEntry();
+    void DeleteExerciseEntry();
+}
